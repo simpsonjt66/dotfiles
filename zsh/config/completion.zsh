da9d81324df34168e477eebf15092f6db0718c3b
John Simpson <simpsonjt66@gmail.com>
2020-10-28 14:07:47 -0400

    refactor: extract completion out to file

---
+++ zsh/config/completion.zsh
@@ -0,0 +1,12 @@
+## Completions
+
+fpath=($HOME/.zsh/completion $fpath)
+
+autoload -U compinit
+compinit
+
+# - Try exact (case-sensitive) match first.
+# - Then fall back to case-insensitive.
+# - Accept abbreviations after . or _ or - (ie. f.b -> foo.bar).
+# - Substring complete (ie. bar -> foobar).
+zstyle ':completion:*' matcher-list '' '+m:{[:lower:]}={[:upper:]}' '+m:{[:upper:]}={[:lower:]}' '+m:{_-}={-_}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
