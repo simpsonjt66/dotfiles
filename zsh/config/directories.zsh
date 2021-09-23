7ea45e123406356f774b32143d134c27c48aa392
John Simpson <simpsonjt66@gmail.com>
2021-09-22 20:03:03 -0400

    Streamline the code

--- zsh/config/directories.zsh
+++ zsh/config/directories.zsh
@@ -4,24 +4,9 @@ setopt autopushd
 setopt pushdignoredups
 setopt pushdminus
 
-alias -- -='cd -'
-alias 1='cd -'
-alias 2='cd -2'
-alias 3='cd -3'
-alias 4='cd -4'
-alias 5='cd -5'
-alias 6='cd -6'
-alias 7='cd -7'
-alias 8='cd -8'
-alias 9='cd -9'
+for index ({1..9}) alias "$index"="cd -${index}"; unset index
 
-function d () {
-  if [[ -n $1 ]]; then
-    dirs "$@"
-  else
-    dirs -v | head -10
-  fi
-}
+alias d='dirs -v'
 
 compdef _dirs d
 
