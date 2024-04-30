ccb502e5cdbc98f72af183e859eba73f182b2978
John Simpson <simpsonjt66@gmail.com>
2024-04-30 16:57:40 -0400

    Moved completion functionality.
    The completion functionality was running and creating two
    compdump files,this has it run with the correct precedence
    and only create the file in the XDG compliant location.

--- zsh/config/.zshrc
+++ zsh/config/.zshrc
@@ -28,7 +28,3 @@ source /usr/share/chruby/auto.sh
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 HISTFILE="$XDG_STATE_HOME"/zsh/history
-# Completion files: Use XDG dirs
-[ -d "$XDG_CACHE_HOME"/zsh ] || mkdir -p "$XDG_CACHE_HOME"/zsh
-zstyle ':completion:*' cache-path "$XDG_CACHE_HOME"/zsh/zcompcache
-compinit -d "$XDG_CACHE_HOME"/zsh/zcompdump-$ZSH_VERSION
--- zsh/config/completion.zsh
+++ zsh/config/completion.zsh
@@ -1,12 +1,17 @@
 ## Completions
 
-fpath=($XDG_CONFIG_HOME/zsh/completion $fpath)
+#fpath=($XDG_CONFIG_HOME/zsh/completion $fpath)
+
+# Completion files: Use XDG dirs
+[ -d "$XDG_CACHE_HOME"/zsh ] || mkdir -p "$XDG_CACHE_HOME"/zsh
+zstyle ':completion:*' cache-path "$XDG_CACHE_HOME"/zsh/zcompcache
 
 autoload -U compinit
-compinit
+compinit -d "$XDG_CACHE_HOME"/zsh/zcompdump-$ZSH_VERSION
+# compinit
 
 # - Try exact (case-sensitive) match first.
 # - Then fall back to case-insensitive.
 # - Accept abbreviations after . or _ or - (ie. f.b -> foo.bar).
 # - Substring complete (ie. bar -> foobar).
-zstyle ':completion:*' matcher-list '' '+m:{[:lower:]}={[:upper:]}' '+m:{[:upper:]}={[:lower:]}' '+m:{_-}={-_}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+# zstyle ':completion:*' matcher-list '' '+m:{[:lower:]}={[:upper:]}' '+m:{[:upper:]}={[:lower:]}' '+m:{_-}={-_}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
