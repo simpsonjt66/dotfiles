9ba174244e8890b2a62978483b58aece6a4969f0
John Simpson <simpsonjt66@gmail.com>
2025-10-08 09:26:45 -0400

    Cleaning up old and unused applications

--- sway/lockman.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Times the screen off and puts it to background
-swayide \
-  timeout 10 'swaymsg "output * dpms off"' \
-  resume 'swaymsg "output * dpms on "' &
-#Locks the screen immediately
-swaylock -c 000000
-# Kills last background task so idle timer doesn't keey running
-kill %%
