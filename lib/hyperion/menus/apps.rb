9a756582f1af0a7be8f39599eba1e210c7082577
John Simpson <simpsonjt66@gmail.com>
2026-05-11 12:58:49 -0400

    Fix rofi launch command to fix bug in launching obsidian

--- lib/hyperion/menus/apps.rb
+++ lib/hyperion/menus/apps.rb
@@ -4,10 +4,11 @@ module Menus
   # Shows rofi in drun mode as an app launcher
   class Apps
     def self.show
-      IO.popen(['rofi',
-                '-show', 'drun',
-                '-run-command', 'uwsm-app {cmd}',
-                '-theme', '~/.config/rofi/themes/app-launcher.rasi'])
+      app_launcher = 'uwsm-app -- {cmd}'
+      system('rofi', '-show',
+             'drun',
+             '-run-command', app_launcher,
+             '-theme', '~/.config/rofi/themes/app-launcher.rasi')
     end
   end
 end
