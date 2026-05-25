5d8f02f9523bdc0452b31fac5576f245d9342180
John Simpson <simpsonjt66@gmail.com>
2026-05-25 01:30:42 -0400

    WIP: Refined the hadling of building theme files
    Still some work to do to integrate them with the theme switcher.

---
+++ lib/hyperion/utilities/color_file_from_alacritty.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'fileutils'
+require 'toml-rb'
+
+module Utilities
+  # Extracts a colorscheme file from existing Alacritty file
+  class ColorFileFromAlacritty
+    def initialize(theme_source)
+      @theme_source = theme_source
+    end
+
+    def validate!
+      raise 'File Exists' if File.exist?(colors_output)
+      raise 'Alacritty.toml file missing' unless File.exist?(alacritty_file)
+    rescue StandardError => e
+      system('notify-send', e.message)
+      exit 1
+    end
+
+    def colors_output
+      @colors_output ||= File.join(@theme_source, 'colors.toml')
+    end
+
+    def alacritty_file
+      @alacritty_file ||= File.join(@theme_source, 'alacritty.toml')
+    end
+
+    def extract
+      validate!
+
+      raw_toml = TomlRB.load_file(alacritty_file, symbolize_keys: true)
+      config = AlacrittyConfig.new(raw_toml)
+
+      write_colors_file(config)
+    end
+
+    def write_colors_file(config)
+      File.write(colors_output, TomlRB.dump(config.to_h))
+    end
+  end
+end
---
+++ lib/hyperion/utilities/theme_set_template.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'toml-rb'
+
+module Utilities
+  # Handles color string transformation (hex, stripped hex, rgb)
+  class ColorTransformer
+    def self.substitutions_for(colors)
+      colors.each_with_object({}) do |(key, value), result|
+        result["{{ #{key} }}"]       = value
+        result["{{ #{key}_strip }}"] = value.delete_prefix('#')
+        result["{{ #{key}_rgb }}"]   = hex_to_rgb(value) if value.to_s.start_with?('#')
+      end
+    end
+
+    def self.hex_to_rgb(hex)
+      r, g, b = hex.delete_prefix('#').scan(/../).map(&:hex)
+      "#{r},#{g},#{b}"
+    end
+  end
+
+  # A generic, efficient regex-based template renderer
+  class TemplateRenderer
+    def initialize(substitutions)
+      @substitutions = substitutions
+      @pattern = Regexp.union(substitutions.keys)
+    end
+
+    def render(text)
+      text.gsub(@pattern, @substitutions)
+    end
+  end
+
+  # Orchestrates the building of the theme files
+  class ThemeSetTemplate
+    TEMPLATES_DIR      = File.join('./templates/').freeze
+    NEXT_THEME_DIR     = File.join('./next_theme/').freeze
+
+    def initialize(theme_dir)
+      @colors_file = File.join(theme_dir, 'colors.toml')
+    end
+
+    def build_config_files
+      return puts "Color file missing: #{@colors_file}" unless File.exist?(@colors_file)
+
+      colors = TomlRB.load_file(@colors_file)
+
+      subs = Utilities::ColorTransformer.substitutions_for(colors)
+      renderer = Utilities::TemplateRenderer.new(subs)
+
+      Dir.glob(File.join(TEMPLATES_DIR, '*.tpl')).each do |tpl_path|
+        output_path = File.join(NEXT_THEME_DIR, File.basename(tpl_path, '.tpl'))
+
+        next if File.exist?(output_path)
+
+        content = File.read(tpl_path)
+        File.write(output_path, renderer.render(content))
+      end
+    end
+  end
+end
